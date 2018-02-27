d91ca392291fe5aca84e8d5f86cd45362f2f89d2
Curtis Mak <curtismak@Curtiss-MacBook-Pro.local>
2018-02-26 16:38:03 -0800

    Adding scrollview features

--- Scrolling/TutorialViewController.m
+++ Scrolling/TutorialViewController.m
@@ -8,15 +8,78 @@
 
 #import "TutorialViewController.h"
 
-@interface TutorialViewController ()
-
+@interface TutorialViewController () <UIScrollViewDelegate>
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) NSMutableArray<UIView*> *pages;
 @end
 
 @implementation TutorialViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.pages = [[NSMutableArray alloc] init];
+    [self.scrollView setPagingEnabled:YES];
+    [self.scrollView setDelegate:self];
+    [self setupPages];
+}
+
+- (void)setupPages{
+    
+    CGFloat buttonWidth = 200;
+    CGFloat buttonHeight = 100;
+    
+    for ( int i = 0; i<4; i++ )
+    {
+        UIView *page = [[UIView alloc] initWithFrame:
+                        CGRectMake(self.view.bounds.size.width * i, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+        
+        
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(self.view.bounds.size.width/2 - buttonWidth/2, self.view.bounds.size.height/2 - buttonHeight/2, buttonWidth, buttonHeight)];
+        
+        [button setTitle:@"Next Page" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(buttonAction) forControlEvents:UIControlEventTouchUpInside];
+        [page addSubview:button];
+        
+        switch (i) {
+            case 0:
+                [page setBackgroundColor:[UIColor greenColor]];
+                break;
+                
+            case 1:
+                [page setBackgroundColor:[UIColor redColor]];
+                break;
+                
+            case 2:
+                [page setBackgroundColor:[UIColor blueColor]];
+                break;
+                
+            case 3:
+                [page setBackgroundColor:[UIColor orangeColor]];
+                break;
+        }
+        [self.pages addObject:page];
+        [self.scrollView addSubview:page];
+    }
+    
+    [self.scrollView setContentSize:CGSizeMake(self.view.bounds.size.width * self.pages.count, 0)];
+}
+
+- (void)buttonAction{
+    
+    CGFloat scrollByHowMuch = self.scrollView.contentOffset.x + self.scrollView.bounds.size.width;
+    [self.scrollView setContentOffset:CGPointMake(scrollByHowMuch, 0) animated:YES];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    for ( int i = 0; i<self.pages.count; i++ )
+    {
+        UIView *page = self.pages[i];
+        CGFloat alpha = ( 1 - ((page.frame.origin.x - scrollView.contentOffset.x)/self.view.frame.size.width));
+        page.alpha = alpha;
+        
+        page.transform = CGAffineTransformMakeScale(alpha, alpha);
+    }
 }
 
 @end
--- Scrolling/ViewController.m
+++ Scrolling/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -18,7 +19,27 @@
     [super viewDidLoad];
     
     [self.navigationController setNavigationBarHidden:YES];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillAppear:) name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
+    self.scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, 1000);
+    
 }
+
+- (void)keyboardWillAppear:(NSNotification*)notification{
+    
+    CGRect keyboardFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    NSLog(@"%@", NSStringFromCGRect(keyboardFrame));
+    
+    [self.scrollView setContentOffset:CGPointMake(0, keyboardFrame.size.height) animated:YES];
+}
+- (void)keyboardWillHide:(NSNotification*)notification{
+    
+    [self.scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
+}
+
 
 - (IBAction)learnMoreAction:(id)sender {
     
